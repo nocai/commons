9a99f19e47626f1f26ce2b8491ac3c72ecc2e693
liujun10 <liujun10@shengpay.com>
2018-07-02 12:56:38 +0800

    add sqlers.go

---
+++ sqlers/sqlers.go
@@ -0,0 +1,63 @@
+package sqlers
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+type Sqler interface {
+	GetSql() string
+	GetCountSql() string
+	GetPageSql(pageNo, pageSize int64) string
+
+	GetArgs() []interface{}
+
+	AppendSqlAndArgs(sql string, arg ...interface{}) Sqler
+}
+
+type BaseSql struct {
+	sql  bytes.Buffer
+	args []interface{}
+}
+
+func (this BaseSql) GetSql() string {
+	return this.sql.String()
+}
+
+func (this BaseSql) GetArgs() []interface{} {
+	return this.args
+}
+
+func (this *BaseSql) AppendSqlAndArgs(sql string, arg ...interface{}) Sqler {
+	this.sql.WriteString(sql)
+	this.args = append(this.args, arg...)
+	return this
+}
+
+func (this *BaseSql) GetCountSql() string {
+	if this.sql.Len() > 0 {
+		return "select count(1) from (" + this.sql.String() + ") as tttttttttttttttttttttt"
+	}
+	panic("sqler err")
+}
+
+func (this *BaseSql) GetPageSql(pageNo, pageSize int64) string {
+	if pageNo > 0 && pageSize > 0 {
+		startIndex := (pageNo - 1) * pageSize
+		return this.GetSql() + " limit " + strconv.FormatInt(startIndex, 10) + ", " + strconv.FormatInt(pageSize, 10)
+	}
+	panic("sqler err")
+}
+
+func (this *BaseSql) String() string {
+	return fmt.Sprintf(`
+		The sql = %s
+		The Args = %q`, this.sql.String(), this.args)
+}
+
+func NewDefaultSqler(sql string) Sqler {
+	var sqler BaseSql
+	sqler.sql.WriteString(sql)
+	return &sqler
+}
