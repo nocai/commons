6a41856c8d79f59660f466e042617f359b4bde18
nocai <395822362@qq.com>
2018-07-16 23:01:58 +0800

    biz_error.go

--- bizerrs/biz_error.go
+++ bizerrs/biz_error.go
@@ -3,22 +3,33 @@ package bizerrs
 type BizError interface {
 	error
 	Code() string
+
+	Cause() error
 }
 
 type bizError struct {
 	code string
 	msg  string
+
+	cause error
 }
 
 func (err bizError) Error() string {
-	return err.msg
+	return err.msg + ":" + err.cause.Error()
 }
 func (err bizError) Code() string {
 	return err.code
 }
+func (err bizError) Cause() error {
+	return err.cause
+}
 
 func New(code, msg string) BizError {
 	return bizError{code, msg}
+}
+
+func Wrap(err BizError, cause error) BizError {
+	return  bizError(err.Code(), err.Error(), cause)
 }
 
 
